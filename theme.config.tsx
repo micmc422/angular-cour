6c3fd7b2588ca97d3fe1fecc3672414bce3b834e
occitaweb <micmc422@gmail.com>
2024-09-02 11:06:36 +0200

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -27,7 +27,13 @@ const config: DocsThemeConfig = {
     icon: <svg className="feather feather-codesandbox" fill="none" height="24" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z" /><polyline points="7.5 4.21 12 6.81 16.5 4.21" /><polyline points="7.5 19.79 7.5 14.6 3 12" /><polyline points="21 12 16.5 14.6 16.5 19.79" /><polyline points="3.27 6.96 12 12.01 20.73 6.96" /><line x1="12" x2="12" y1="22.08" y2="12" /></svg>
   },
   footer: {
-    text: 'Champollion - TW3 - angular - Bardy Michaël',
+    text: (<span>
+        Champollion - TW3 - angular - {new Date().getFullYear()} ©{' '} -
+        <a href="https://nextra.site" target="_blank">
+          Bardy Michaël
+        </a>
+        .
+      </span>)
   },
   gitTimestamp: ({ timestamp }) => <>Dernière mise à jour {timestamp.toLocaleDateString()}</>
 }
